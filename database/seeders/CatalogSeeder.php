e234dc73a3fe868ef0f326f64bca0d4563c46708
TheRogueShon <47004552+TheRogueShon@users.noreply.github.com>
2021-10-11 15:57:54 -0400

    Add files via upload
    Team 1 updated catalog seeder with the information found on solar systems.

--- database/seeders/CatalogSeeder.php
+++ database/seeders/CatalogSeeder.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Seeder;
 use DB;
 
 class CatalogSeeder extends Seeder
-
 {
     /**
      * Run the database seeds.
@@ -16,16 +15,37 @@ class CatalogSeeder extends Seeder
     public function run()
     {
         DB::table('catalogs')->insert([
-			[
-				'name' => 'Solar Panel',
-				'description' => '300 Watt panel',
-				'price' => '1000.00'
-			],
-			[
-				'name' => 'Solar Panel Inverter',
-				'description' => '1800VA',
-				'price' => '3000.00'
-			]
-	]);
+            
+                [
+                    'company' => 'Solar Energy Innovations Inc',
+                    'name'=> 'Magnum Energy inverter',
+                    'description' => '4kW, 120/240V, 60Hz, Sine Wave/ Off-grid: 120/240V',
+                    'price' => '1000.00'
+                ],
+                [
+                    'company' => 'Solar Energy Innovations Inc',
+                    'name'=> 'SMA Sunny Island',
+                    'description' => '5kW @ 230V AC, 50Hz Off-grid',
+                    'price' => '1000.00'
+                ],
+                [
+                    'company' => 'Innogen Solar Electricity',
+                    'name'=> '2kW System',
+                    'description' => '2kW, Insurance: Free lifetime servicing with app monitoring, Use grid at night',
+                    'price' => '9700.00'
+                ],
+                [
+                    'company' => 'Innogen Solar Electricity',
+                    'name'=> '4kW System',
+                    'description' => '2kW, Insurance: Free lifetime servicing with app monitoring, Use grid at night',
+                    'price' => '16000.00'
+                ],
+                [
+                    'company' => 'Solar Watt Systems Inc',
+                    'name'=> 'Photovoltage system',
+                    'description' => '5kW, No insurance',
+                    'price' => '15000.00'
+                ]
+            ]);
     }
 }
