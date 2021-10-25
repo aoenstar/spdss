6c8e9eb6ed69a5e176537a101eee7a1d82fe5b1b
TheRogueShon <47004552+TheRogueShon@users.noreply.github.com>
2021-10-25 14:55:00 -0400

    Updated show

--- resources/views/catalog/show.blade.php
+++ resources/views/catalog/show.blade.php
@@ -1,13 +1,35 @@
 <html>
-<head>
-	<title>Catalog</title>
-</head>
+    <head>
+        <title>View</title>
+    </head>
+    @extends('layouts.app')
 
-<body>
-	<div>
-		<h2>Catalog Item:  {{ $item->name}}: ${{ $item->price }}</h2>
-		<p>{{ $item->description }}</p>
-	</div>
-</body>
+    @section('content')
 
+        <div class="container">
+            <div class="row justify-content-center">
+                <div class="col-md-8">
+                    <div class="card">
+                        <h2 class = "card-header text-center">Viewing Catalog Item: {{$item->id}}</h2>
+                        <div class="card-body justify-content-center">
+                            <p>Item Name: {{$item->name}}</p>
+                            <p>Price: ${{$item->price}} </p> 
+                            <p>Item: Description: {{$item->description}}</p>
+                            <p>Item sold by: {{$item->company}}</p>
+
+                            {!! Form::open(['method' => 'DELETE', 'url' => '/catalog/' .
+                                $item->id]) !!}
+                                <p class="text-center">
+                                    {!! Form::submit('Delete', ['class' => 'btn btn-danger']) !!}
+                                </p>
+
+                            {!! Form::close() !!}
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+       
+    @endsection
 </html>
