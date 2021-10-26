0505e39f7e756fbb5c0178216b9e563964669d95
shabazzbynoe <shabazz_96@hotmail.com>
2021-10-26 13:18:38 -0400

    Add files via upload

---
+++ resources/views/aboutus.blade.php
@@ -0,0 +1,103 @@
+@extends('layouts.app')
+
+@section('content')
+<!-- Styles -->
+<style>
+        body
+        {
+
+        }
+
+        .container
+        {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+        }   
+        
+        .card
+        {
+            background: white;
+            width: 500px;
+            margin: 10px;
+            border-radius: 15px;
+        }
+
+        .card-image
+        {
+            
+            height: 150px;
+            width: 500px;
+            margin-bottom:15px;
+            background-size:cover;
+            border-radius: 5%;
+        }
+
+        .image1
+        {
+            background-image: url('https://www.treehugger.com/thmb/1GDveahdHGkPO29NhBKuJPgniIw=/4368x2457/smart/filters:no_upscale()/house-with-solar-panels-174673282-ab883d486cb043a985efd2f5020381d0.jpg');
+        }
+
+        .image2
+        {
+            background-image: url('https://image.freepik.com/free-photo/tax-concept-1040-tax-form-with-calculator-desk-finance-time_359031-12451.jpg');
+        }
+
+        .image3
+        {
+            background-image: url('https://sunpower.maxeon.com/au/sites/default/files/styles/hero_image_1920_auto/public/2021-07/Sunpower-maxeon-common-problems-solar-panels-roof-issues_hero%20.jpg?itok=5Emei5i2');
+        }
+
+        .card h2
+        {
+           text-align: center;
+        }
+
+        .card p
+        {
+            height: 110px;
+            padding-bottom: 20px;
+            font-size :18px;
+        }
+
+        .background1
+        {
+            background-image: url('https://solarworksca.com/wp-content/uploads/2018/01/banner-finance-incentives.jpg');
+            height: 200px;
+            background-size: cover;
+            font-size: 20px;
+            text-align: justify;
+        }
+
+        .background1 h1
+        {
+            padding-top: 70px;
+            color: white;
+        }
+    </style>
+    <div class="background1">
+        <h1 style="text-align: center">About Us</h1>
+    </div>    
+    <section class="container">
+        <div class="card">
+            <h2>Decison Support System</h2>
+            <div class = "card-image image1"></div>
+            <p>A Decision Support System to help individuals and businesses make decisions about the type of Solar Power System that they can afford to install in their homes or offices.One goal of our website is to recommend an appropriate solar power system to suit your needs by working closely alongside Solar Power companies here in Barbados.</p>
+        </div>
+
+        <div class="card">
+            <h2>Price Calculator</h2>
+            <div class = "card-image image2"></div> 
+            <p>Our price calculator accurately determines the cost of your desired Solar Power System including auxillary costs, individual cost of parts and insurance.</p>          
+        </div>
+
+        <div class="card">
+            <h2>Catalog</h2>
+            <div class = "card-image image3"></div>         
+            <p>A database of all of our current available systems.</p>
+        </div>
+    </section>
+    <h1 style="text-align: center">Get Your Solar System Today!</h1>
+@endsection
+
+
---
+++ resources/views/contacts.blade.php
@@ -0,0 +1,98 @@
+@extends('layouts.app')
+
+@section('content')
+    <!-- Styles -->
+    <style>
+        body
+        {
+            background: lightgrey;
+        }
+
+        .container
+        {
+            display: flex;
+            justify-content: center;
+            flex-wrap: wrap;
+        }   
+        
+        .card
+        {
+            background: white;
+            width: 270px;
+            margin: 10px;
+            border-radius: 15px;
+            text-align: center;
+        }
+
+        .card-image
+        {
+            height: 150px;
+            width: 270px;
+            margin-bottom:15px;
+            background-color: white;
+            background-size:cover;
+            border-radius: 5%;
+        }
+
+        .card-image img
+        {
+            height: 150px;
+            width: 270px;
+            border-radius: 5%;
+        }
+
+        .card h2
+        {
+            padding: 10px;
+        }
+
+        .card p
+        {
+            height: 110px;
+            padding-bottom: 20px;
+        }
+
+        .card a
+        {
+            background-color: black;
+            color: white;
+            padding: 15px 20px;
+            display: block;
+            text-align: center;
+            margin: 20px 50px;
+            text-decoration: none;
+        }
+        .card:hover
+        {
+            background-color: rgb(62,62,62);
+            color: white;
+            cursor: pointer;
+            transform: scale(1.03);
+            transition: all 1s ease;
+        }
+
+
+    </style>
+    <h1 style="text-align: center">Contacts</h1>
+    <section class="container">
+        <div class="card">
+            <div class = "card-image"><img src="https://www.innogenonline.com/wp-content/uploads/2018/04/Innogen-logo-new.png"></div>
+            <h2>Innogen Solar Electricity</h2>
+
+            <p>Phone: +1 246 228-2107 <br><br> Email: sales@innogenonline.com  <br><br> Address: Harold Hoyte and Fred Gollop Media Centre,Fontabelle, St. Michael,Bridgetown, Barbados</p>
+            <a href="">Contact Now</a>
+        </div>
+        <div class="card">
+            <div class = "card-image"><img src="https://seisolarinc.com/wp-content/uploads/2020/10/seisolarinclogo.jpg"></div>
+            <h2>Solar Energy Innovations Inc</h2>
+            <p>Phone: +1 246-271-1330 <br><br> Email: office@simmons.bb  <br><br> Address: Lot C 16 Emerald Park, St Philip, Barbados</p>
+            <a href="">Contact Now</a>
+        </div>
+        <div class="card">
+            <div class = "card-image"><img src="https://solarwattsystems.com/wp-content/themes/solarwatt_systemsinc/assets/img/sw_logo%20copy.png"></div>
+            <h2>Solar Watt <br> Systems Inc</h2>
+            <p>Phone: +1 246-622-1450 <br><br> Email: info@solarwattsystems.com  <br><br> Address: Barracks Hill No.1, Oistins BB15071, Barbados</p>
+            <a href="">Contact Now</a>
+        </div>
+    </section>
+@endsection
